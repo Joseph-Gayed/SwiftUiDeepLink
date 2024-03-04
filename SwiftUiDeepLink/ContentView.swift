0840f32beb2798929027201aa27bda22e85f6a33
Jo Gayed <joseph.gayed91@gmail.com>
2024-03-04 11:06:54 +0200

    initial empty project

---
+++ SwiftUiDeepLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiDeepLink
+//
+//  Created by Jo on 04/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("SwiftUiDeepLink!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiDeepLink/SwiftUiDeepLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiDeepLinkApp.swift
+//  SwiftUiDeepLink
+//
+//  Created by Jo on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiDeepLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
