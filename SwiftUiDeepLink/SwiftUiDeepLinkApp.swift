0842f417954e39da849293cbb304f430ecf6ba24
Jo Gayed <joseph.gayed91@gmail.com>
2024-03-05 02:18:10 +0200

    Configure deep link schema in info.plist and Handle deep link to change the tab according to the tab in the link.

--- SwiftUiDeepLink/SwiftUiDeepLinkApp.swift
+++ SwiftUiDeepLink/SwiftUiDeepLinkApp.swift
@@ -15,7 +15,46 @@ struct SwiftUiDeepLinkApp: App {
     var body: some Scene {
         WindowGroup {
             AppContentView()
-                .environmentObject(appData) //This allows all child views of AppContentView to access and observe changes to the appData object.
+                //This allows all child views of AppContentView to access and observe changes to the appData object.
+                .environmentObject(appData)
+                
+                //This will be called when deep link was triggered
+                .onOpenURL(perform: { url in
+                    handleDeepLink(link: url.absoluteString)
+                })
+        }
+    }
+    
+    //go to project settings , in the info tab , under the URL types section , check the the schema.
+    //For changing the tab expected urls:
+    //    swiftuideeplink://tab=home
+    //    swiftuideeplink://tab=favorites
+    //    swiftuideeplink://tab=settings
+    
+    //For changing the navigation inside a tab expected urls:
+    //    swiftuideeplink://tab=home&&nav=My Posts
+    func handleDeepLink(link:String){
+        print(link)
+        
+        //split link by "?"
+        let components = link.replacingOccurrences(of: "swiftuideeplink://", with: "").components(separatedBy: "?")
+        
+        for component in components{
+            //tab change request
+            if component.contains("tab="){
+                let tabRawValue = component.replacingOccurrences(of: "tab=", with: "")
+                print(tabRawValue)
+
+                if let requestedTab = Tab.convert(from: tabRawValue){
+                    appData.activeTab = requestedTab
+                }
+            }
+            
+            //nav change request
+            if component.contains("tab=") && component.contains("nav="){
+                let tabRawValue = component
+            }
+            
         }
     }
 }
